26e0fd7d2e22283d18daa17c5c6682ac1630ee10
sharky <zeebing@gmail.com>
2019-06-12 17:12:47 -0400

    first push

---
+++ js/app.js
@@ -0,0 +1,38 @@
+
+let menuState = "closed";
+
+$(document).ready(function() {
+    responsiveSizing($(window).width());
+
+    $(window).resize(function() {
+        let viewportWidth = $(window).width();
+        responsiveSizing(viewportWidth);
+    })
+    $("#menu-toggle").on("click", () => {
+        toggleMenu();
+    })
+})
+
+toggleMenu = () => {
+    if(menuState === "closed"){
+        $("#menu-toggle").attr("aria-label","Close menu").text("close");
+        $("#menu").attr("style","display: block");
+        menuState = "open";
+    }
+    else{
+        $("#menu-toggle").attr("aria-label","Open menu").text("open");
+        $("#menu").attr("style","display: none");
+        menuState = "closed";
+    }
+}
+
+responsiveSizing = (width) => {
+    if (width <= 640) {
+        $("#menu-toggle").show();
+        $("#menu").attr("style", "display: none");
+    }
+    else{
+        $("#menu-toggle").hide();
+        $("#menu").attr("style", "display: block");
+    }
+}
